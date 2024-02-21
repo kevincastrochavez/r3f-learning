f936ab0621c5d8b082356d5f479b4d1896063768
Kevin Castro <cas20057@byui.edu>
2024-02-21 16:52:00 -0700

    learned how to add directional light and ambient light

--- src/Experience.js
+++ src/Experience.js
@@ -7,7 +7,7 @@ function Experience() {
   const groupRef = useRef(null);
   const { camera, gl } = useThree(); // Used to get the camera and other stuff
 
-  extend({ OrbitControls });
+  extend({ OrbitControls }); // Since OrbitControls is a class not built in we extend it
 
   useFrame((state, delta) => {
     // console.log(delta);
@@ -19,21 +19,25 @@ function Experience() {
   return (
     <>
       <orbitControls args={[camera, gl.domElement]} />
+
+      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={0.5} />
+
       <group ref={groupRef}>
         <mesh position={[-2, -0.5, 0]}>
           <sphereGeometry args={[0.5, 32, 32]} />
-          <meshBasicMaterial wireframe color='hotpink' />
+          <meshStandardMaterial wireframe color='hotpink' />
         </mesh>
 
         <mesh ref={cubeRef} position={[2, -0.5, 0]}>
           <boxGeometry scale={1.5} />
-          <meshBasicMaterial wireframe color='purple' />
+          <meshStandardMaterial color='purple' />
         </mesh>
       </group>
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry args={[1, 1]} />
-        <meshBasicMaterial color='green' />
+        <meshStandardMaterial color='green' />
       </mesh>
     </>
   );
