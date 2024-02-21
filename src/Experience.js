ab7de9d4cf8404da6ed2012a142f006517407b9b
Kevin Castro <cas20057@byui.edu>
2024-02-21 16:48:17 -0700

    learned how to use the orbit controls

--- src/Experience.js
+++ src/Experience.js
@@ -1,19 +1,24 @@
 import React, { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
+import { useFrame, extend, useThree } from '@react-three/fiber';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 function Experience() {
   const cubeRef = useRef(null);
   const groupRef = useRef(null);
+  const { camera, gl } = useThree(); // Used to get the camera and other stuff
+
+  extend({ OrbitControls });
 
   useFrame((state, delta) => {
     // console.log(delta);
     // cubeRef.current.rotation.x += delta;
     // cubeRef.current.rotation.y += delta;
-    groupRef.current.rotation.y += delta;
+    // groupRef.current.rotation.y += delta;
   });
 
   return (
     <>
+      <orbitControls args={[camera, gl.domElement]} />
       <group ref={groupRef}>
         <mesh position={[-2, -0.5, 0]}>
           <sphereGeometry args={[0.5, 32, 32]} />
