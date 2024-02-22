8804a9e6619311c302c0bb315194143c407dc298
Kevin Castro <cas20057@byui.edu>
2024-02-22 16:43:43 -0700

    installed drei for new way to implement orbit controls, and created file copy

--- src/Experience.js
+++ src/Experience.js
@@ -1,42 +1,21 @@
 import React, { useRef } from 'react';
-import { useFrame, extend, useThree } from '@react-three/fiber';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import Custom from './Custom';
+import { OrbitControls } from '@react-three/drei';
 
 function Experience() {
-  const cubeRef = useRef(null);
-  const groupRef = useRef(null);
-  const { camera, gl } = useThree(); // Used to get the camera and other stuff
-
-  extend({ OrbitControls }); // Since OrbitControls is a class not built in we extend it
-
-  useFrame((state, delta) => {
-    // console.log(delta);
-    // cubeRef.current.rotation.x += delta;
-    // cubeRef.current.rotation.y += delta;
-    // groupRef.current.rotation.y += delta;
-    // console.log(state.clock.elapsedTime);
-    // CAMERA
-    // const angle = state.clock.elapsedTime;
-    // state.camera.position.x = Math.sin(angle) * 8;
-    // state.camera.position.z = Math.cos(angle) * 8;
-    // state.camera.lookAt(0, 0, 0);
-  });
-
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
+      <OrbitControls />
 
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={0.5} />
 
-      <group ref={groupRef}>
+      <group>
         <mesh position={[-2, -0.5, 0]}>
           <sphereGeometry args={[0.5, 32, 32]} />
           <meshStandardMaterial wireframe color='hotpink' />
         </mesh>
 
-        <mesh ref={cubeRef} position={[2, -0.5, 0]}>
+        <mesh position={[2, -0.5, 0]}>
           <boxGeometry scale={1.5} />
           <meshStandardMaterial color='purple' />
         </mesh>
@@ -47,7 +26,7 @@ function Experience() {
         <meshStandardMaterial color='green' />
       </mesh>
 
-      <Custom />
+      {/* <Custom /> */}
     </>
   );
 }
---
+++ src/ExperienceCopy.js
@@ -0,0 +1,55 @@
+import React, { useRef } from 'react';
+import { useFrame, extend, useThree } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+// import Custom from './Custom';
+
+function Experience() {
+  const cubeRef = useRef(null);
+  const groupRef = useRef(null);
+  const { camera, gl } = useThree(); // Used to get the camera and other stuff
+
+  // extend({ OrbitControls }); // Since OrbitControls is a class not built in we extend it
+
+  // useFrame((state, delta) => {
+  //   // console.log(delta);
+  //   // cubeRef.current.rotation.x += delta;
+  //   // cubeRef.current.rotation.y += delta;
+  //   // groupRef.current.rotation.y += delta;
+  //   // console.log(state.clock.elapsedTime);
+  //   // CAMERA
+  //   // const angle = state.clock.elapsedTime;
+  //   // state.camera.position.x = Math.sin(angle) * 8;
+  //   // state.camera.position.z = Math.cos(angle) * 8;
+  //   // state.camera.lookAt(0, 0, 0);
+  // });
+
+  return (
+    <>
+      <OrbitControls />
+
+      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={0.5} />
+
+      <group ref={groupRef}>
+        <mesh position={[-2, -0.5, 0]}>
+          <sphereGeometry args={[0.5, 32, 32]} />
+          <meshStandardMaterial wireframe color='hotpink' />
+        </mesh>
+
+        <mesh ref={cubeRef} position={[2, -0.5, 0]}>
+          <boxGeometry scale={1.5} />
+          <meshStandardMaterial color='purple' />
+        </mesh>
+      </group>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry args={[1, 1]} />
+        <meshStandardMaterial color='green' />
+      </mesh>
+
+      {/* <Custom /> */}
+    </>
+  );
+}
+
+export default Experience;
