e8bc4875fa6860be27b773c6c07579aa05c58178
Kevin Castro <cas20057@byui.edu>
2024-02-21 17:42:39 -0700

    started creating custom object and used useMemo to optimize the drawing of vertices

---
+++ src/Custom.js
@@ -0,0 +1,40 @@
+import React, { useMemo } from 'react';
+import { extend, useThree } from '@react-three/fiber';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from 'three';
+
+function Custom() {
+  const verticesCount = 10 * 3;
+
+  const { camera, gl } = useThree(); // Used to get the camera and other stuff
+  extend({ OrbitControls }); // Since OrbitControls is a class not built in we extend it
+
+  const positions = useMemo(() => {
+    const positions = new Float32Array(verticesCount * 3); // 3 because x, y, z for each vertice
+
+    for (let i = 0; i < verticesCount * 3; i++) {
+      positions[i] = (Math.random() - 0.5) * 3;
+    }
+
+    return positions;
+  }, []);
+
+  return (
+    <>
+      <orbitControls args={[camera, gl.domElement]} />
+      <mesh>
+        <bufferGeometry>
+          <bufferAttribute
+            attach='attributes-position'
+            count={verticesCount}
+            itemSize={3}
+            array={positions}
+          />
+        </bufferGeometry>
+        <meshBasicMaterial color='red' side={THREE.DoubleSide} />
+      </mesh>
+    </>
+  );
+}
+
+export default Custom;
--- src/object3d.js
+++ src/object3d.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import Experience from './Experience';
+import Custom from './Custom';
 
 function Object3d() {
   return (
     <Canvas>
-      <Experience />
+      {/* <Experience /> */}
+      <Custom />
     </Canvas>
   );
 }
